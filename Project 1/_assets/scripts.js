a12dd34e8c3718df739ad15849b0ce431f445626
Sanjana Navar <navas916@newschool.edu>
2019-03-06 13:45:48 -0500

    mass commit

---
+++ Project 1/_assets/scripts.js
@@ -0,0 +1,37 @@
+// This is a comment
+
+//Beginning
+
+$(document).ready(function(){
+
+//stuff happens here
+
+$('.indefinite').on('click', function(){
+
+	//dblclick for double click
+
+	$('.hexgallery1').addClass('hexgallery');
+
+
+});
+
+//$('.mybox1').on('mouseleave', function(){
+
+	//dblclick for double click
+
+	//$('.mybox1').removeClass('makebig');
+	//$('.mybox3').removeClass('makebig');
+//	$('.mybox2').removeClass('makebig');
+	//$('.mybox2').removeClass('disappear');
+
+
+});
+//similar to hover- if you want it to stop when youre not on the box anymore
+
+//$('.mybox3').on('dblclick', function(){
+
+	//$('.mybox1').append('<h1 class="yo">This is a header tag</h1>');
+
+	//$('.mybox2').html('REPLACED TEXT');
+
+//End
